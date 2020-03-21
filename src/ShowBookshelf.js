bfde0543c573090e35b4e1e94318992d36fb7ed2
Michal <michalwit111@gmail.com>
2020-03-21 11:12:27 +0100

    feat: add dummy 'moveBook' operation

--- src/ShowBookshelf.js
+++ src/ShowBookshelf.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import ListBooksContent from './ListBooksContent'
@@ -7,13 +8,18 @@ class ShowBookshelf extends React.Component {
 
   render() {
     return (
-        <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
-          </div>
-          <ListBooksContent/>
-          <Link to="/search">Add a book</Link>
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
         </div>
+        <ListBooksContent
+          currentlyReading={this.props.currentlyReading}
+          wantToRead={this.props.wantToRead}
+          read={this.props.read}
+          onMoveBook={this.props.onMoveBook}
+        />
+        <Link to="/search" className="open-search">Add a book</Link>
+      </div>
     )
   }
 }

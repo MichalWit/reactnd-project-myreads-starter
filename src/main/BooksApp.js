5a0d1244a87372ed17a810fdae42ade35f77a2bf
Michal <michalwit111@gmail.com>
2020-03-22 08:04:37 +0100

    feat: persist state in localstorage

--- src/main/BooksApp.js
+++ src/main/BooksApp.js
@@ -13,44 +13,34 @@ export const Shelf = {
 
 class BooksApp extends React.Component {
 
-  state = {
-    bookshelfs: [
+  buildEmptyBookshelfs = () => {
+    return (
       {
-        id: Shelf.CURRENTLY_READING,
-        books: [
-          {
-            image: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
-            title: "To Kill a Mockingbird",
-            authors: ["Harper Lee"]
-          },
-          {
-            authors: ["Mike Hendrickson", "Brian Sawyer"],
-            title: "Best Android Apps",
-            image: "http://books.google.com/books/content?id=bUybAgAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
-          }
-        ]
-      },
-      {
-        id: Shelf.WANT_TO_READ,
-        books: [
-          {
-            image: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
-            title: "1776",
-            authors: ["David McCullough"]
-          }
-        ]
-      },
-      {
-        id: Shelf.READ,
-        books: [
-          {
-            image: "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api",
-            title: "The Hobbit",
-            authors: ["J.R.R. Tolkien"]
-          }
+        bookshelfs: [
+          { id: Shelf.CURRENTLY_READING, books: [] },
+          { id: Shelf.WANT_TO_READ, books: [] },
+          { id: Shelf.READ, books: [] }
         ]
       }
-    ]
+    )
+  }
+
+  state = this.buildEmptyBookshelfs()
+
+  componentDidMount = () => {
+    let retrievedState = localStorage.state;
+    let parsedState = null;
+    if (!retrievedState)
+      parsedState = this.buildEmptyBookshelfs()
+    else
+      parsedState = JSON.parse(retrievedState)
+    this.setState(() => (
+      parsedState
+    ))
+  }
+
+  componentDidUpdate = () => {
+    localStorage.setItem('state', JSON.stringify(this.state))
   }
 
   partition = (toPartition, predicate) => {
